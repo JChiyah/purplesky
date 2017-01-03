1413de7224aa1b56d9ad97dbcacc5d54abae7859
Javier Chiyah <fjc3@hw.ac.uk>
2017-01-03 20:08:29 +0000

    Whoops...

--- application/views/login.php
+++ application/views/login.php
@@ -1,39 +1,60 @@
-<div class="container">
-	<!--setting the grid container to center-->
-	<div class="col-xs-2 col-xs-offset-5 col-lg-2 col-lg-offset-5">
-		<div class="row">
-			<h1>Sign in</h1>
+<?php
+  defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en-GB">
+  <head>
+    <meta charset="utf-8">
+    <meta name="description" content=" <?php echo $des ?? '' ?> ">
+    <meta name="keywords" content="">
+    <meta name="viewport" content="initial-scale=1">
 
-      <div id="infoMessage"><?php echo $message;?></div>
+    <title><?php echo $title ?? '' ?></title>
+    
+    <script src="https://use.fontawesome.com/eedb59a6cd.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 
-      <?php echo form_open("login");?>
+    <link rel="stylesheet" href="<?php echo base_url("assets/css/style.css"); ?>" />
+  </head>
+  <body>
+    <div class="container">
+    	<!--setting the grid container to center-->
+    	<div class="col-xs-2 col-xs-offset-5 col-lg-2 col-lg-offset-5">
+    		<div class="row">
+    			<h1>Sign in</h1>
 
-        <p>
-          <?php echo lang('login_identity_label', 'identity');?>
-          <?php echo form_input($identity);?>
-        </p>
+          <div id="infoMessage"><?php echo $message;?></div>
 
-        <p>
-          <?php echo lang('login_password_label', 'password');?>
-          <?php echo form_input($password);?>
-        </p>
+          <?php echo form_open("login");?>
 
-        <p>
-          <?php echo lang('login_remember_label', 'remember');?>
-          <?php echo form_checkbox('remember', '1', FALSE, 'id="remember"');?>
-        </p>
+            <p>
+              <?php echo lang('login_identity_label', 'identity');?>
+              <?php echo form_input($identity);?>
+            </p>
+
+            <p>
+              <?php echo lang('login_password_label', 'password');?>
+              <?php echo form_input($password);?>
+            </p>
+
+            <p>
+              <?php echo lang('login_remember_label', 'remember');?>
+              <?php echo form_checkbox('remember', '1', FALSE, 'id="remember"');?>
+            </p>
 
 
-        <p><?php echo form_submit('submit', lang('login_submit_btn'));?></p>
+            <p><?php echo form_submit('submit', lang('login_submit_btn'));?></p>
 
-      <?php echo form_close();?>
+          <?php echo form_close();?>
 
-      <p><a href="auth/forgot_password"><?php echo lang('login_forgot_password');?></a></p>
-		</div>
-		<div class="row">
-			<!--TODO-->
-			<img src="assets/img/leidos-logo.png" alt="Leidos logo">
-			<p>Powered by Purple Sky 2016</p>
-		</div>
-	</div>
-</div>
+          <p><a href="auth/forgot_password"><?php echo lang('login_forgot_password');?></a></p>
+    		</div>
+    		<div class="row">
+    			<!--TODO-->
+    			<img src="assets/img/leidos-logo.png" alt="Leidos logo">
+    			<p>Powered by Purple Sky 2016</p>
+    		</div>
+    	</div>
+    </div>
+  </body>
+</html>
