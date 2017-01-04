42cf7416879c2463c2a7f1c233dcdd710cad128f
Javier Chiyah <fjc3@hw.ac.uk>
2017-01-04 02:30:32 +0000

    Responsive footer
    Footer completely responsive for now. Do not touch!

--- application/views/inc/footer.php
+++ application/views/inc/footer.php
@@ -1,26 +1,28 @@
 <footer>
-	<div class="col-md-4"></div>
-	<div class="col-md-4">
-		<div class="row">
-		    <ul>
-		      <li><a href="<?php echo site_url('index') ?>">Home</a></li>-
-		      <li><a href="<?php echo site_url('index') ?>">Projects</a></li>-
-		      <li><a href="<?php echo site_url('profile') ?>">Profile</a></li>-
-		      <li><a href="<?php echo site_url('index') ?>">Search</a></li>-
-		      <li><a href="<?php echo site_url('index') ?>">Help</a></li>
-		    </ul>
+	<div id="footer">
+		<div class="hidden-xs col-md-4"></div>
+		<div class="col-sm-12 col-md-4">
+			<div class="row">
+			    <ul>
+			      <li><a href="<?php echo site_url('index') ?>">Home</a></li>-
+			      <li><a href="<?php echo site_url('index') ?>">Projects</a></li>-
+			      <li><a href="<?php echo site_url('profile') ?>">Profile</a></li>-
+			      <li><a href="<?php echo site_url('index') ?>">Search</a></li>-
+			      <li><a href="<?php echo site_url('index') ?>">Help</a></li>
+			    </ul>
+			</div>
+			<div class="row">
+			    <ul id="footer-bottom">
+			      <li><a href="<?php echo site_url('index') ?>">Privacy Policy</a></li>|
+			      <li><a href="<?php echo site_url('index') ?>">Terms of Use</a></li>|
+			      <li><a id="logout" href="<?=  base_url('auth/logout')?>">Logout</a></li>
+			    </ul>
+			</div>
 		</div>
-		<div class="row">
-		    <ul id="footer-bottom">
-		      <li><a href="<?php echo site_url('index') ?>">Privacy Policy</a></li>|
-		      <li><a href="<?php echo site_url('index') ?>">Terms of Use</a></li>|
-		      <li><a id="logout" href="<?=  base_url('auth/logout')?>">Logout</a></li>
-		    </ul>
+		<div id="footer-right" class="col-sm-12 col-md-4">
+			<img id="footer-logo" src="assets/img/leidos-white.png" alt="Leidos logo">
+		    <p>Powered by Purple Sky &#169 2016</p>
 		</div>
-	</div>
-	<div id="footer-right" class="col-md-4">
-		<img id="footer-logo" src="assets/img/leidos-white.png" alt="Leidos logo">
-	    <p>Powered by Purple Sky &#169 2016</p>
 	</div>
 </footer>
 <!--<script type="text/javascript" src="inc/fnc/site.js"></script>-->
