789f0b4fb1fe776233b4283811083b3d64ebd20d
Mobeen Aftab <mobeenaftab1@gmail.com>
2017-01-09 22:51:45 +0000

    Added projectSummary - incomplete
    incomplete, basic structure only -

---
+++ application/views/projectSummary.php
@@ -0,0 +1,35 @@
+<!--TODO::double check what types each form element should be-->
+<div class="container">
+  <h3>Step 3: Project Summary</h3>
+  <hr>
+  <p></p> <!--Are these headings or lables with data that is pulled from DB?-->
+  <h1>Project name here</h1> <!--Glyphy with edit icon-->
+  <h2>Project manager name here</h2>
+  <div class="row"> <!--Start and priority-->
+    <div class="col-xs-12 col-sm-8 col-md-8">
+
+    </div>
+    <div class="col-xs-12 col-sm-8 col-md-8">
+
+    </div> <!--Start and priority end-->
+  </div>
+  <div class="row"> <!--end and location-->
+    <div class="col-xs-12 col-sm-8 col-md-8">
+
+    </div>
+    <div class="col-xs-12 col-sm-8 col-md-8">
+
+    </div>
+  </div> <!--end and location end-->
+  <p></p>
+  <div class="row">
+    <div class="col-xs-12 col-sm-12 col-md-12">
+      <sub>Brief description of project here</sub>
+    </div>
+  </div>
+  <p></p>
+  <div class="row"><!--Seleted staff-->
+      <!--TODO:: how are we displaying this? using a table?-->
+    </div>
+  </div>
+</div><!--container end-->
