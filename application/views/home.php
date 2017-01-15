ca41e846d4fa06a0b964c3ad47421ff75e2dcdf4
Mobeen Aftab <mobeenaftab1@gmail.com>
2017-01-15 22:22:31 +0000

    Styled pages
    added style to these pages, require finishing touches.

--- application/views/home.php
+++ application/views/home.php
@@ -1,82 +1,82 @@
-<!--TODO::Go over this with the team to decide what element every item needs to be because
-some of the display functions requires access to the databse/user data and that can be displayed in
-many ways using JS, HTML etc.-->
-
-<div class="container">
-	<br>
-	<!--Notification and date row-->
-	<div class="row">
-		<div class="col-xs-12 col-md-6 col-lg-7">Notifications
-			<hr>
-			<!--How are going to implement the notification system? using a table?-->
-			<table>
-				<tr>
-					<th>
-						Date Time
-					</th>
-				</tr>
-				<tr>
-					<th>
-						Notification at "Project Link".
-					</th>
-				</tr>
-				<tr>
-					<th>
-						Date Time
-					</th>
-				</tr>
-				<tr>
-					<th>
-						Notification at "Project Link".
-					</th>
-				</tr>
-			</table>
-		</div>
-		<div class="col-xs-12 col-md-6 col-lg-5">Date
-			<br>
-			<hr>
-			<!--Calander implementation-->
-			<p>Calander placeHolder</p>
-		</div>
-	</div>
-	<br>
-	<!--Current projects and Recomended for you-->
-	<div class="row">
-		<div class="col-xs-12 col-md-6 col-lg-5">Current Projects
-			<hr>
-			<br>
-			<!--current project show as buttons/links? - replace link with var the comes from DB-->
-			<button type="button" name="Project" onclick="location.href='http://google.com'">Project A</button>
-			<button type="button" name="Project" onclick="location.href='http://google.com'">Project B</button>
-			<button type="button" name="Project" onclick="location.href='http://google.com'">Project C</button>
-		</div>
-		<!--Recomended Section -->
-		<div class="col-xs-12 col-md-6 col-lg-7">Recomended for you
-			<hr>
-			<br>
-			<!--Create a new row here and divide the space evenly for each project section
-					3 cols each, leaves border space for arrows and styling-->
-			<!--info glyph icon needed-->
-			<div class="row">
-				<div class="col-xs-12 col-md-3 col-lg-5">
-					<!--Bootstrap will let you nest grids within themselfs, use that-->
-					<!--div class box containter needed-->
-					<h3>ProjectD</h3>
-					<p>Description</p>
-					<p>Location</p>
-					<p>Project Manager</p>
-					<p>From "StartDate" to "EndDate"</p>
-				</div>
-				<div class="col-xs-12 col-md-3 col-lg-5">
-					<!--div class box containter needed-->
-					<h3>ProjectE</h3>
-					<p>Description</p>
-					<p>Location</p>
-					<p>Project Manager</p>
-					<p>From "StartDate" to "EndDate"</p>
-				</div>
+<div id="homePage">
+	<div class="container">
+		<br>
+		<!--Notification and date row-->
+		<div class="row">
+			<div class="col-xs-12 col-md-6 col-lg-7" id="HomeContainer">
+				<h2>Notifications</h2>
+				<hr>
+				<!--How are going to implement the notification system? using a table?-->
+				<table>
+					<tr>
+						<th>
+							Date Time
+						</th>
+					</tr>
+					<tr>
+						<th>
+							Notification at "Project Link".
+						</th>
+					</tr>
+					<tr>
+						<th>
+							Date Time
+						</th>
+					</tr>
+					<tr>
+						<th>
+							Notification at "Project Link".
+						</th>
+					</tr>
+				</table>
+			</div>
+			<div class="col-xs-12 col-md-6 col-lg-5" id="HomeContainer">
+				<h2>Date</h2>
+				<hr>
+				<!--Calander implementation-->
+				<p>Calander placeHolder</p>
 			</div>
 		</div>
-	</div>
-</div>	<!--Container end-->
+		<br>
+		<!--Current projects and Recomended for you-->
+		<div class="row">
+			<div class="col-xs-12 col-md-6 col-lg-4" id="HomeContainer">
+				<h2>Current Projects</h2>
+				<hr>
+				<!--current project show as buttons/links? - replace link with var the comes from DB-->
+				<button type="button" name="Project" onclick="location.href='http://google.com'">Project A</button><br>
+				<button type="button" name="Project" onclick="location.href='http://google.com'">Project B</button><br>
+				<button type="button" name="Project" onclick="location.href='http://google.com'">Project C</button><br>
+			</div>
+			<!--Recomended Section -->
+			<div class="col-xs-12 col-md-6 col-lg-8" id="HomeContainer">
+				<h2>Recomended for you</h2>
+				<hr>
+				<!--TODO fix CSS here for displaying projects - requ gliphys-->
+				<div class="row" id="HomeContainer">
+					<div class="col-xs-12 col-md-6 col-lg-8">
+						<div class="row">
+							<div class="col-xs-12 col-ms-3 col-md-3 col-lg-4" id="shift-left">
+								<h2>ProjectD</h2>
+								<p>Description</p>
+								<p>Location</p>
+								<p>Project Manager</p>
+								<p>From "StartDate" to "EndDate"</p>
+							</div>
+							<div class="col-xs-12 col-ms-3 col-md-3 col-lg-4" id="shift-right">
+								<h2>ProjectE</h2>
+								<p>Description</p>
+								<p>Location</p>
+								<p>Project Manager</p>
+								<p>From "StartDate" to "EndDate"</p>
+							</div>
+						</div>
+					</div>
+			</div>
+		</div>
+
+	</div>	<!--Container end-->
+</div>
 <p><a href="auth/change_password">Change password</a></p>
+<?php echo anchor('auth/create_user', lang('index_create_user_link'))?>
+<p><a href="<?php echo site_url('register') ?>">Create profile</a></p>
--- application/views/profile.php
+++ application/views/profile.php
@@ -1,74 +1,98 @@
-<!-- Simple grid example
-Check https://v4-alpha.getbootstrap.com/layout/grid/
-for more information on the grid system and layout
-
-Check http://getbootstrap.com/css/#responsive-utilities
-for responsive tools and classes, such as .hidden-xs
-
-SIZE CHART GUIDE
-    md - medium   | some tablets and small desktops
-    lg - large    | desktops
--->
-
-<div class="container">
-	<div class="row">
-		<!-- A row with 2 columns -->
-		<div style="background: white" class="col-lg-5">
-			<h1>Employee</h1>
-			<hr>
-			<!--double check should these be labels or text fields? or do they turn into text fields after edit-->
-			<p>Employee email</P>
-			<p>account role</p>
-			<p>Location</p>
-			<hr>
-			<br>
-			<from action="link_blank"><!--Maybe use a ref link to next page?-->
-				<input type="submit" value="ChangePassword" />
-			</from>
-		</div>
-		<br>
-		<!--Skill section-->
-		<div style="background: white" class="col-lg-5">
-			<h1>Skils</h1>
-			<hr>
-			<div class="form-group">
-				<label for="Skill">Skill:</label>
-				<input type="text" class="form-control" id="Skill">
+<div id="userProfile">
+	<div class="container">
+		<div class="row">
+			<!-- A row with 2 columns -->
+			<div class="col-xs-12 col-sm-6 col-md-6 col-lg-5">
+				<h1>Employee</h1>
+				<!--label ? double check should these be labels or text fields? or do they turn into text fields after edit-->
+				<p>Employee email</P>
+				<p>account role</p>
+				<p>Location</p>
+				<hr>
+				<form class="" action="index.html" method="post">
+					<input type="submit" name="changePassword" value="Change Password">
+				</form>
 			</div>
-			<br>
-			<label for="skillArea">Skill Selection:</label><br>
-			<textarea class="form-control" rows="5" id="skillArea"></textarea>
+			<!--Skill section-->
+			<div class="col-xs-12 col-sm-6 col-md-6 col-lg-7">
+				<h1>Skils</h1>
+				<hr>
+				<div id="UPContainer">
+					<form class="" action="index.html" method="post">
+						<div class="row">
+							<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+								<input type="text" name="skills" value="Type Skills here">
+							</div>
+							<div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+								<a href="#">Add</a>
+							</div>
+						</div>
+						<!--div class="row">
+							<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6"-->
+							<label>Click on one or multiple skills to remove skills(s)</label>
+								<textarea name="skillArea" rows="8" cols="250"></textarea>
+							<!--/div>
+						</div-->
+							<div class="row">
+								<div class="col-md-2 offset-md-10">
+									<a href="#">Remove</a>
+								</div>
+							</div>
+					</form>
+				</div>
+			</div>
 		</div>
-	</div>
 
-	<!--Exp section-->
-	<h1>Experience</h1>
-	<hr>
-	<div class="row">
-		<div style="background: white" class="col-xs-12 col-md-8">Title:
-			<form>
-				<input type="text" name="Title" value="">
+		<!--Exp section-->
+		<!--TODO: come back later and nest the To and FROM date in a single nested row under title to clean up-->
+		<h1>Experience</h1>
+		<hr>
+		<div id="UPContainer">
+			<form class="" action="index.html" method="post">
+				<div class="row">
+					<div class="col-xs-12 col-md-8">
+						<label>Title:</label><br>
+						<input type="text" name="Title" value="">
+					</div>
+					<div class="col-xs-12 col-md-4">
+						<label>From:</label>
+						<input type="text" name="From" value="DD/MM/YYYY">
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-xs-12 col-md-8">
+					</div>
+					<div class="col-xs-12 col-md-4">
+						<label>To: </label>
+							<input type="text" name="From" value="DD/MM/YYYY">
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-xs-12 col-md-12">
+						<label>Description:</label><br>
+						<textarea name="Description" rows="5" cols="100"></textarea>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-xs-12 col-md-6">
+						<label>Skills Developed:</label><br>
+						<input type="text" name="skillsDeveloped" value="Type skill here...">
+					</div>
+					<div class="col-xs-12 col-md-2">
+						<a href="#">Add</a>
+					</div>
+					<div class="col-xs-12 col-md-4">
+						<input type="submit" name="addExp" value="Add Experience">
+					</div>
+				</div>
 			</form>
 		</div>
-		<div style="background: white" class="col-xs-12 col-md-4">From
-			<form>
-				<input type="text" name="From" value="DD/MM/YYYY">
-			</form>
-		</div>
-	</div>
-	<div class="row">
-		<div style="background: white" class="col-xs-12 col-md-8"></div>
-		<div style="background: white" class="col-xs-12 col-md-4">To:
-			<form>
-				<input type="text" name="From" value="DD/MM/YYYY">
-			</form>
-		</div>
-	</div>
-	<div class="row">
-		<div style="background: white" class="col-xs-12 col-md-12">Description:
-			<form>
-				<textarea name="Description" rows="5" cols="100"></textarea>
-			</form>
-		</div>
+		<!--TODO:: ask to find out how to implement this exact section, below is just a placeholder
+		add a div class to display top and bottom block lines, dont use hr-->
+		<hr>
+		<h1>Title of Experience</h1>
+		<label>Description...</label>
+		<label>Skills developed</label>
+		<hr>
 	</div>
 </div>
