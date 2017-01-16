cc4b3108f6400fb68b6080a57908e0ab68f45a96
Mobeen Aftab <mobeenaftab1@gmail.com>
2017-01-16 22:42:40 +0000

    add skills styled
    add skills page fully styled
    update with scss and css file

--- application/views/addSkills.php
+++ application/views/addSkills.php
@@ -1,33 +1,43 @@
-<!--TODO::Does this page include a header/footer?-->
-<div class="container">
-  <h1>Skill</h1>
-  <div class="row">
-    <h3>Add some skills to your account so we can find the best projects for you !</h3>
-    <div class="col-xs-12 col-sm-8 col-md-8">
-      <p></p>
-      <form class="" action="index.html" method="post">
-        <input type="text" name="skill" value="Begin typing skill here...">
-        <p></p>
-    </div>
-    <div class="col-xs-12 col-sm-8 col-md-4">
-      <!--TODO::Glyphy with add function-->
-      <sub>Add</sub>
-    </div>
-    <p></p>
+<div id="addSkill">
+  <div class="container" id="ASContainer">
+    <h1>Skill</h1>
     <div class="row">
+      <p>Add some skills to your account so we can find the best projects for you !</p>
       <div class="col-xs-12 col-sm-8 col-md-8">
         <p></p>
-        <!--TODO::box should contain previously saved/recently added skill-->
-        <textarea rows="5" cols="50" name="savedskill" value="" required></textarea>
+        <form class="" action="index.html" method="post">
+          <input type="text" name="skill" value="Begin typing skill here...">
+          <p></p>
       </div>
       <div class="col-xs-12 col-sm-8 col-md-4">
-        <!--TODO::Glyphy with remove function-->
-        <sub>Remove</sub>
+        <!--TODO::Glyphy with add function-->
+        <a href="#">Add</a>
       </div>
+      <p></p>
+      <div class="row">
+        <div class="col-xs-12 col-sm-8 col-md-8">
+          <p></p>
+          <!--TODO::box should contain previously saved/recently added skill-->
+          <textarea rows="8" cols="80" name="savedskill" value=""></textarea>
+        </div>
+        <div class="col-xs-12 col-sm-8 col-md-4">
+          <!--TODO::Glyphy with remove function-->
+          <a href="#">Remove</a>
+        </div>
+      </div>
+      <p></p>
+      <div class="row" id="submit-row">
+        <div class="col-xs-12 col-sm-6 col-md-6">
+          <input type="submit" name="complete" value="complete">
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-6">
+          <div id="login-logo">
+            <img src="assets/img/leidos-logo.png" alt="Leidos logo">
+            <p>Powered by Purple Sky &#169 2016</p>
+         </div>
+        </div>
+      </div>
+      </form>
     </div>
-    <p></p>
-    <!--TODO shift button left, add logo to right-->
-      <input type="submit" name="Load" value="COMPLETE">
-    </form>
-  </div>
-</div><!--container end-->
+  </div><!--container end-->
+</div>
