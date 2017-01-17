28db856ae0cb7baa1b0a4f112df91f33bc901782
Javier Chiyah <fjc3@hw.ac.uk>
2017-01-17 23:18:28 +0000

    Fix footer
    Fixing a bug with the footer

--- application/views/create-project.php
+++ application/views/create-project.php
@@ -1,51 +1,53 @@
-
-  <div id="newProject">
-    <div class="container">
+<div id="newProject">
+   <div class="container">
       <h1>Create new Project</h1>
-    <!--TODO:: apply correct fonts with bold -->
+      <!--TODO:: apply correct fonts with bold -->
       <div class="row">
-        <h2>Step 1: Project Details</h2>
-        <hr>
-        <div class="col-xs-12 col-sm-6 col-md-12" id="NPcontainer">
-          <p>Please fill out all fields.</p>
-          <form class="" action="index.html" method="post">
-            <label>Title:</label> <br>
-            <input type="text" name="title" value="" required>
-            <p></p>
-            <label>Description:</label><br>
-            <textarea rows="5" cols="50" name="" value="" required></textarea>
-            <p></p>
-            <label>From:</label>
-            <input type="date" name="fromDate" value="DD/MM/YYYY">
-            <p></p>
-            <label>To:</label>
-            <input type="date" name="toDate" value="DD/MM/YYYY">
-            <p></p>
-            <label>Location:</label><br>
-            <select name="location">
-              <!--TODO pull data from database for drop down selection - need to validate input(required)-->
-              <option value="select">Select</option>
-              <option value="test 1">Test 1</option>
-              <option value="test 2">Test 2</option>
-            </select>
-            <p></p>
-            <!--TODO::fix css misplacment error-->
-            <!--div class="row">
-              <div class="col-xs-12 col-sm-6 col-md-5"-->
-                <label>Priority:</label> <br>
-                <select name="priority">
-                  <option value="normal">Normal</option>
-                  <option value="high">High</option>
-                </select>
-              <!--/div-->
-              <div class="row">
-                <div class="col-xs-12 col-sm-6 col-md-offset-7">
-                  <input type="submit" name="submit" value="continue">
-                </div>
-              </div>
-            <!--/div-->
-          </form>
-        <div>
+         <h2>Step 1: Project Details</h2>
+         <hr>
+         <div class="col-xs-12 col-sm-6 col-md-12" id="NPcontainer">
+            <p>Please fill out all fields.</p>
+            <form class="" action="index.html" method="post">
+               <p>
+                  <label>Title:</label> <br>
+                  <input type="text" name="title" value="" required>
+               </p>
+               <p>
+                  <label>Description:</label><br>
+                  <textarea rows="5" cols="50" name="" value="" required></textarea>
+               </p>
+               <p>
+                  <label>From:</label>
+                  <input type="date" name="fromDate" value="DD/MM/YYYY">
+               </p>
+               <p>
+                  <label>To:</label>
+                  <input type="date" name="toDate" value="DD/MM/YYYY">
+               </p>
+               <p>
+                  <label>Location:</label><br>
+                  <select name="location">
+                  <!--TODO pull data from database for drop down selection - need to validate input(required)-->
+                    <option value="select">Select</option>
+                    <option value="test 1">Test 1</option>
+                    <option value="test 2">Test 2</option>
+                  </select>
+               </p>
+               <!--TODO::fix css misplacment error-->
+               <!--div class="row">
+                 <div class="col-xs-12 col-sm-6 col-md-5"-->
+               <p>
+                  <label>Priority:</label> <br>
+                  <select name="priority">
+                     <option value="normal">Normal</option>
+                     <option value="high">High</option>
+                  </select>
+               </p>
+
+               <input type="submit" name="submit" value="continue">
+
+            </form>
+         </div>
       </div>
-    </div>
-  </div>
+   </div>
+</div>
