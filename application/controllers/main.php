52b792b0d5a4071cfe14fc1dfa7f114ceaa9982b
Javier Chiyah <fjc3@hw.ac.uk>
2017-02-06 17:51:28 +0000

    More...

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -34,7 +34,7 @@ class Main extends CI_Controller {
 	public function index()
 	{
 		$this->load->helper('url_helper');
-    
+	 
 		$d['body'] = 'home';
 		$d['title'] = 'Home';
 		$d['des'] = 'Homepage with dashboard';
@@ -48,39 +48,39 @@ class Main extends CI_Controller {
 		$d['des'] = 'User profile';
 
 		$d['skill_select'] = array(
-         'name'  => 'skill_select',
-         'id'    => 'skill_select',
-         'value' => $this->form_validation->set_value('skill_select'),
-      );
-      $d['skills'] = $this->User_model->get_skills();
+			'name'  => 'skill_select',
+			'id'    => 'skill_select',
+			'value' => $this->form_validation->set_value('skill_select'),
+		);
+		$d['skills'] = $this->User_model->get_skills();
 
-      // User-related data
-      $user_id = $this->session->userdata('user_id');
-      $user = $this->ion_auth->user($user_id)->row();
-      $user_groups = $this->ion_auth->get_users_groups($user_id)->row();
+		// User-related data
+		$user_id = $this->session->userdata('user_id');
+		$user = $this->ion_auth->user($user_id)->row();
+		$user_groups = $this->ion_auth->get_users_groups($user_id)->row();
 
-      if($user_groups->id == 1 || $user_groups->id == 2) {
-      	redirect('index');
-      }
+		if($user_groups->id == 1 || $user_groups->id == 2) {
+			redirect('index');
+		}
 
-      $d['user'] = array(
-      	'name' 		=> $user->first_name . ' ' . $user->last_name,
-      	'email'		=> $user->email,
-      	'group'		=> $user_groups->description,
-      	'location' 	=> $this->User_model->get_user_location($user_id)->name,
-      	'skills' 	=> $this->User_model->get_user_skills($user_id)
-	   );
+		$d['user'] = array(
+			'name' 		=> $user->first_name . ' ' . $user->last_name,
+			'email'		=> $user->email,
+			'group'		=> $user_groups->description,
+			'location' 	=> $this->User_model->get_user_location($user_id)->name,
+			'skills' 	=> $this->User_model->get_user_skills($user_id)
+		);
 
 		$this->load->view('html', $d);
 	}
 
 	public function create_project_view()
 	{
-    $user_groups = $this->ion_auth->get_users_groups($this->session->userdata('user_id'))->row();
+	 $user_groups = $this->ion_auth->get_users_groups($this->session->userdata('user_id'))->row();
 
-    if($user_groups->id != 1 && $user_groups->id != 2) {
-      redirect('index');
-    }
+	 if($user_groups->id != 1 && $user_groups->id != 2) {
+		redirect('index');
+	 }
 
 		$d['body'] = 'create-project';
 		$d['title'] = 'Create project';
@@ -107,236 +107,236 @@ class Main extends CI_Controller {
   // change password
   public function change_password()
   {
-    $this->form_validation->set_rules('old', $this->lang->line('change_password_validation_old_password_label'), 'required');
-    $this->form_validation->set_rules('new', $this->lang->line('change_password_validation_new_password_label'), 'required|min_length[' . $this->config->item('min_password_length', 'ion_auth') . ']|max_length[' . $this->config->item('max_password_length', 'ion_auth') . ']|matches[new_confirm]');
-    $this->form_validation->set_rules('new_confirm', $this->lang->line('change_password_validation_new_password_confirm_label'), 'required');
+	 $this->form_validation->set_rules('old', $this->lang->line('change_password_validation_old_password_label'), 'required');
+	 $this->form_validation->set_rules('new', $this->lang->line('change_password_validation_new_password_label'), 'required|min_length[' . $this->config->item('min_password_length', 'ion_auth') . ']|max_length[' . $this->config->item('max_password_length', 'ion_auth') . ']|matches[new_confirm]');
+	 $this->form_validation->set_rules('new_confirm', $this->lang->line('change_password_validation_new_password_confirm_label'), 'required');
 
-    if (!$this->ion_auth->logged_in())
-    {
-      redirect('login', 'refresh');
-    }
+	 if (!$this->ion_auth->logged_in())
+	 {
+		redirect('login', 'refresh');
+	 }
 
-    $user = $this->ion_auth->user()->row();
+	 $user = $this->ion_auth->user()->row();
 
-    if ($this->form_validation->run() == false)
-    {
-      // display the form
-      // set the flash data error message if there is one
-      $this->data['message'] = (validation_errors()) ? validation_errors() : $this->session->flashdata('message');
+	 if ($this->form_validation->run() == false)
+	 {
+		// display the form
+		// set the flash data error message if there is one
+		$this->data['message'] = (validation_errors()) ? validation_errors() : $this->session->flashdata('message');
 
-      $this->data['min_password_length'] = $this->config->item('min_password_length', 'ion_auth');
-      $this->data['old_password'] = array(
-        'name' => 'old',
-        'id'   => 'old',
-        'type' => 'password',
-      );
-      $this->data['new_password'] = array(
-        'name'    => 'new',
-        'id'      => 'new',
-        'type'    => 'password',
-        'pattern' => '^.{'.$this->data['min_password_length'].'}.*$',
-      );
-      $this->data['new_password_confirm'] = array(
-        'name'    => 'new_confirm',
-        'id'      => 'new_confirm',
-        'type'    => 'password',
-        'pattern' => '^.{'.$this->data['min_password_length'].'}.*$',
-      );
-      $this->data['user_id'] = array(
-        'name'  => 'user_id',
-        'id'    => 'user_id',
-        'type'  => 'hidden',
-        'value' => $user->id,
-      );
+		$this->data['min_password_length'] = $this->config->item('min_password_length', 'ion_auth');
+		$this->data['old_password'] = array(
+		  'name' => 'old',
+		  'id'   => 'old',
+		  'type' => 'password',
+		);
+		$this->data['new_password'] = array(
+		  'name'    => 'new',
+		  'id'      => 'new',
+		  'type'    => 'password',
+		  'pattern' => '^.{'.$this->data['min_password_length'].'}.*$',
+		);
+		$this->data['new_password_confirm'] = array(
+		  'name'    => 'new_confirm',
+		  'id'      => 'new_confirm',
+		  'type'    => 'password',
+		  'pattern' => '^.{'.$this->data['min_password_length'].'}.*$',
+		);
+		$this->data['user_id'] = array(
+		  'name'  => 'user_id',
+		  'id'    => 'user_id',
+		  'type'  => 'hidden',
+		  'value' => $user->id,
+		);
 
-      // render
-      $this->data['body'] = 'auth/change_password';
-      $this->load->view('html', $this->data);
-    }
-    else
-    {
-      $identity = $this->session->userdata('identity');
+		// render
+		$this->data['body'] = 'auth/change_password';
+		$this->load->view('html', $this->data);
+	 }
+	 else
+	 {
+		$identity = $this->session->userdata('identity');
 
-      $change = $this->ion_auth->change_password($identity, $this->input->post('old'), $this->input->post('new'));
+		$change = $this->ion_auth->change_password($identity, $this->input->post('old'), $this->input->post('new'));
 
-      if ($change)
-      {
-        //if the password was successfully changed
-        /*
-        $this->session->set_flashdata('message', $this->ion_auth->messages());
-        $this->logout();*/
-        redirect('profile', 'refresh');
-      }
-      else
-      {
-        $this->session->set_flashdata('message', $this->ion_auth->errors());
-        // render
-        redirect('password', 'refresh');
-      }
-    }
+		if ($change)
+		{
+		  //if the password was successfully changed
+		  /*
+		  $this->session->set_flashdata('message', $this->ion_auth->messages());
+		  $this->logout();*/
+		  redirect('profile', 'refresh');
+		}
+		else
+		{
+		  $this->session->set_flashdata('message', $this->ion_auth->errors());
+		  // render
+		  redirect('password', 'refresh');
+		}
+	 }
   }
 
   // forgot password
   public function forgot_password()
   {       
-    $this->form_validation->set_rules('identity', $this->lang->line('forgot_password_validation_email_label'), 'required|valid_email');
+	 $this->form_validation->set_rules('identity', $this->lang->line('forgot_password_validation_email_label'), 'required|valid_email');
 
-    if ($this->form_validation->run() == false)
-    {
-      $this->data['type'] = $this->config->item('identity','ion_auth');
-      // setup the input
-      $this->data['identity'] = array('name' => 'identity',
-        'id' => 'identity',
-      );
+	 if ($this->form_validation->run() == false)
+	 {
+		$this->data['type'] = $this->config->item('identity','ion_auth');
+		// setup the input
+		$this->data['identity'] = array('name' => 'identity',
+		  'id' => 'identity',
+		);
 
-      if ( $this->config->item('identity', 'ion_auth') != 'email' ){
-        $this->data['identity_label'] = $this->lang->line('forgot_password_identity_label');
-      }
-      else
-      {
-        $this->data['identity_label'] = $this->lang->line('forgot_password_email_identity_label');
-      }
+		if ( $this->config->item('identity', 'ion_auth') != 'email' ){
+		  $this->data['identity_label'] = $this->lang->line('forgot_password_identity_label');
+		}
+		else
+		{
+		  $this->data['identity_label'] = $this->lang->line('forgot_password_email_identity_label');
+		}
 
-      // set any errors and display the form
-      $this->data['message'] = (validation_errors()) ? validation_errors() : $this->session->flashdata('message');
-      
-      // render
-      $this->data['body'] = 'auth/forgot_password';
-      $this->load->view('html', $this->data);
-    }
-    else
-    {
-      $identity_column = $this->config->item('identity','ion_auth');
-      $identity = $this->ion_auth->where($identity_column, $this->input->post('identity'))->users()->row();
+		// set any errors and display the form
+		$this->data['message'] = (validation_errors()) ? validation_errors() : $this->session->flashdata('message');
+		
+		// render
+		$this->data['body'] = 'auth/forgot_password';
+		$this->load->view('html', $this->data);
+	 }
+	 else
+	 {
+		$identity_column = $this->config->item('identity','ion_auth');
+		$identity = $this->ion_auth->where($identity_column, $this->input->post('identity'))->users()->row();
 
-      if(empty($identity)) {
+		if(empty($identity)) {
 
-                  if($this->config->item('identity', 'ion_auth') != 'email')
-                  {
-                    $this->ion_auth->set_error('forgot_password_identity_not_found');
-                  }
-                  else
-                  {
-                     $this->ion_auth->set_error('forgot_password_email_not_found');
-                  }
+						if($this->config->item('identity', 'ion_auth') != 'email')
+						{
+						  $this->ion_auth->set_error('forgot_password_identity_not_found');
+						}
+						else
+						{
+							$this->ion_auth->set_error('forgot_password_email_not_found');
+						}
 
-                    $this->session->set_flashdata('message', $this->ion_auth->errors());
-                    redirect("passforgot", 'refresh');
-                }
+						  $this->session->set_flashdata('message', $this->ion_auth->errors());
+						  redirect("passforgot", 'refresh');
+					 }
 
-      // run the forgotten password method to email an activation code to the user
-      $forgotten = $this->ion_auth->forgotten_password($identity->{$this->config->item('identity', 'ion_auth')});
+		// run the forgotten password method to email an activation code to the user
+		$forgotten = $this->ion_auth->forgotten_password($identity->{$this->config->item('identity', 'ion_auth')});
 
-      if ($forgotten)
-      {
-        // if there were no errors
-        $this->session->set_flashdata('message', $this->ion_auth->messages());
-        //redirect("login", 'refresh'); //we should display a confirmation page here instead of the login page
-      }
-      else
-      {
-        $this->session->set_flashdata('message', $this->ion_auth->errors());
-        redirect("passforgot", 'refresh');
-      }
-    }
+		if ($forgotten)
+		{
+		  // if there were no errors
+		  $this->session->set_flashdata('message', $this->ion_auth->messages());
+		  //redirect("login", 'refresh'); //we should display a confirmation page here instead of the login page
+		}
+		else
+		{
+		  $this->session->set_flashdata('message', $this->ion_auth->errors());
+		  redirect("passforgot", 'refresh');
+		}
+	 }
   }
 
 	// Helper function to create the register form
 	public function create_user_form()
 	{
 		// display the create user form
-        // set the flash data error message if there is one
+		  // set the flash data error message if there is one
 		$this->data['message'] = (validation_errors() ? validation_errors() : ($this->ion_auth->errors() ? $this->ion_auth->errors() : $this->session->flashdata('message')));
 
-      $this->data['first_name'] = array(
-         'name'  => 'first_name',
-         'id'    => 'first_name',
-         'type'  => 'text',
-         'value' => $this->form_validation->set_value('first_name'),
-      );
-      $this->data['last_name'] = array(
-         'name'  => 'last_name',
-         'id'    => 'last_name',
-         'type'  => 'text',
-         'value' => $this->form_validation->set_value('last_name'),
-      );
-      $this->data['email'] = array(
-         'name'  => 'email',
-         'id'    => 'email',
-         'type'  => 'text',
-         'value' => $this->form_validation->set_value('email'),
-      );
-      $this->data['password'] = array(
-         'name'  => 'password',
-         'id'    => 'password',
-         'type'  => 'password',
-         'value' => $this->form_validation->set_value('password'),
-      );
-      $this->data['password_confirm'] = array(
-         'name'  => 'password_confirm',
-         'id'    => 'password_confirm',
-         'type'  => 'password',
-         'value' => $this->form_validation->set_value('password_confirm'),
-      );
-      $this->data['groups'] = array(
-         'name'  => 'groups',
-         'id'    => 'groups',
-         'value' => $this->form_validation->set_value('groups'),
-      );
+		$this->data['first_name'] = array(
+			'name'  => 'first_name',
+			'id'    => 'first_name',
+			'type'  => 'text',
+			'value' => $this->form_validation->set_value('first_name'),
+		);
+		$this->data['last_name'] = array(
+			'name'  => 'last_name',
+			'id'    => 'last_name',
+			'type'  => 'text',
+			'value' => $this->form_validation->set_value('last_name'),
+		);
+		$this->data['email'] = array(
+			'name'  => 'email',
+			'id'    => 'email',
+			'type'  => 'text',
+			'value' => $this->form_validation->set_value('email'),
+		);
+		$this->data['password'] = array(
+			'name'  => 'password',
+			'id'    => 'password',
+			'type'  => 'password',
+			'value' => $this->form_validation->set_value('password'),
+		);
+		$this->data['password_confirm'] = array(
+			'name'  => 'password_confirm',
+			'id'    => 'password_confirm',
+			'type'  => 'password',
+			'value' => $this->form_validation->set_value('password_confirm'),
+		);
+		$this->data['groups'] = array(
+			'name'  => 'groups',
+			'id'    => 'groups',
+			'value' => $this->form_validation->set_value('groups'),
+		);
 
-      return $this->data;
+		return $this->data;
 	}
 
 	// create a new user
 	public function create_user()
-   {
-      $this->data['title'] = $this->lang->line('create_user_heading');
+	{
+		$this->data['title'] = $this->lang->line('create_user_heading');
 
-      $tables = $this->config->item('tables','ion_auth');
-     	$identity_column = $this->config->item('identity','ion_auth');
-  		$this->data['identity_column'] = $identity_column;
+		$tables = $this->config->item('tables','ion_auth');
+		$identity_column = $this->config->item('identity','ion_auth');
+		$this->data['identity_column'] = $identity_column;
 
-      // validate form input
-      $this->form_validation->set_rules('first_name', $this->lang->line('create_user_validation_fname_label'), 'required');
-     	$this->form_validation->set_rules('last_name', $this->lang->line('create_user_validation_lname_label'), 'required');
-     
-     	$this->form_validation->set_rules('email', $this->lang->line('create_user_validation_email_label'), 'required|valid_email|is_unique[' . $tables['users'] . '.email]');
-     
-     	$this->form_validation->set_rules('password', $this->lang->line('create_user_validation_password_label'), 'required|min_length[' . $this->config->item('min_password_length', 'ion_auth') . ']|max_length[' . $this->config->item('max_password_length', 'ion_auth') . ']|matches[password_confirm]');
-     	$this->form_validation->set_rules('password_confirm', $this->lang->line('create_user_validation_password_confirm_label'), 'required');
+		// validate form input
+		$this->form_validation->set_rules('first_name', $this->lang->line('create_user_validation_fname_label'), 'required');
+		$this->form_validation->set_rules('last_name', $this->lang->line('create_user_validation_lname_label'), 'required');
+	  
+		$this->form_validation->set_rules('email', $this->lang->line('create_user_validation_email_label'), 'required|valid_email|is_unique[' . $tables['users'] . '.email]');
+	  
+		$this->form_validation->set_rules('password', $this->lang->line('create_user_validation_password_label'), 'required|min_length[' . $this->config->item('min_password_length', 'ion_auth') . ']|max_length[' . $this->config->item('max_password_length', 'ion_auth') . ']|matches[password_confirm]');
+		$this->form_validation->set_rules('password_confirm', $this->lang->line('create_user_validation_password_confirm_label'), 'required');
 
-     	if ($this->form_validation->run() == true)
-     	{
-         $email    = strtolower($this->input->post('email'));
-         $password = $this->input->post('password');
+		if ($this->form_validation->run() == true)
+		{
+			$email    = strtolower($this->input->post('email'));
+			$password = $this->input->post('password');
 
-         $additional_data = array(
-            'first_name' => $this->input->post('first_name'),
-            'last_name'  => $this->input->post('last_name'),
-         );
+			$additional_data = array(
+				'first_name' => $this->input->post('first_name'),
+				'last_name'  => $this->input->post('last_name'),
+			);
 
-         $groups = array($this->input->post('groups')+1);
-     	}
-     	if ($this->form_validation->run() == true && $this->ion_auth->register($password, $email, $additional_data, $groups))
-     	{
-         // check to see if we are creating the user
-         // redirect them back to the admin page
-         $this->session->set_flashdata('message', $this->ion_auth->messages());
-         redirect("login", 'refresh');
-     	}
-     	else
-     	{
-         $data = $this->create_user_form();
+			$groups = array($this->input->post('groups')+1);
+		}
+		if ($this->form_validation->run() == true && $this->ion_auth->register($password, $email, $additional_data, $groups))
+		{
+			// check to see if we are creating the user
+			// redirect them back to the admin page
+			$this->session->set_flashdata('message', $this->ion_auth->messages());
+			redirect("login", 'refresh');
+		}
+		else
+		{
+			$data = $this->create_user_form();
 
 			$this->load->view('register', $this->data);
-     	}
- 	}
+		}
+	}
 
 	public function add_user_skill() {
 		$skill = $this->input->post('skill');
 
-      // find the current users id
-      $id = $this->session->userdata('user_id');
+		// find the current users id
+		$id = $this->session->userdata('user_id');
 
 		// send value to database
 		if ($this->User_model->add_skill($skill, $id)) {
@@ -353,12 +353,12 @@ class Main extends CI_Controller {
 	public function delete_skill() {
 		$skill = $this->input->post('delete_skill');
 
-      // find the current users id
-      $id = $this->session->userdata('user_id');
+		// find the current users id
+		$id = $this->session->userdata('user_id');
 
-      if($this->User_model->delete_user_skill($skill, $id)) {
-      	echo 'success';
-      }
+		if($this->User_model->delete_user_skill($skill, $id)) {
+			echo 'success';
+		}
 	}
 
 }
--- application/models/user_model.php
+++ application/models/user_model.php
@@ -5,135 +5,135 @@ class User_model extends CI_Model {
 
 	/* Validates a user and retrieves its details based on an email and password */
 	function validate_user( $email, $password ) {
-        $this->db->from('account');
-        $this->db->where('email',$email );
-        $this->db->where( 'password', sha1($password) );
-        
-        // Results of the query
-        $login = $this->db->get()->result();
+		$this->db->from('account');
+		$this->db->where('email',$email );
+		$this->db->where( 'password', sha1($password) );
+		
+		// Results of the query
+		$login = $this->db->get()->result();
 
-        // If a value exists, then the user account exists and is validated
-        if ( is_array($login) && count($login) == 1 ) {
-            // Set the users details into the $details property of this class
-            $this->details = $login[0];
-            // Call set_session to set the user's session vars via CodeIgniter
-            $this->set_session();
-            return true;
-        }
+		// If a value exists, then the user account exists and is validated
+		if ( is_array($login) && count($login) == 1 ) {
+			// Set the users details into the $details property of this class
+			$this->details = $login[0];
+			// Call set_session to set the user's session vars via CodeIgniter
+			$this->set_session();
+			return true;
+		}
 
-        return false;
-    }
+		return false;
+	}
 
-    public function get_user_location($id=FALSE) {
-        // if no id was passed use the current users id
-        $id = isset($id) ? $id : $this->session->userdata('user_id');
+	public function get_user_location($id=FALSE) {
+		// if no id was passed use the current users id
+		$id = isset($id) ? $id : $this->session->userdata('user_id');
 
-        $query = $this->db->select()
-                        ->where('staff_id', $id)
-                        ->join('location', 'location.location_id=staff.current_location')
-                        ->get('staff');
+		$query = $this->db->select()
+						->where('staff_id', $id)
+						->join('location', 'location.location_id=staff.current_location')
+						->get('staff');
 
-        $result = $query->row();
+		$result = $query->row();
 
-        if(isset($result) && !empty($result)) {
-            return $result;
-        }
-        return FALSE;
-    }
+		if(isset($result) && !empty($result)) {
+			return $result;
+		}
+		return FALSE;
+	}
 
-    public function get_skills() {
+	public function get_skills() {
 
-        $query = $this->db->select('name')->order_by('skill_id', 'desc')->get('skill');
+		$query = $this->db->select('name')->order_by('skill_id', 'desc')->get('skill');
 
-        $array = array();
-        foreach ($query->result() as $row) {
-            $array[] = $row->name;
-        }
+		$array = array();
+		foreach ($query->result() as $row) {
+			$array[] = $row->name;
+		}
 
-        return array_reverse($array);
+		return array_reverse($array);
 
-    }
+	}
 
-    public function add_skill($skill_name, $id=FALSE) {
-        // if no id was passed use the current users id
-        $id = isset($id) ? $id : $this->session->userdata('user_id');
+	public function add_skill($skill_name, $id=FALSE) {
+		// if no id was passed use the current users id
+		$id = isset($id) ? $id : $this->session->userdata('user_id');
 
-        if(!isset($skill_name)) {
-            return FALSE;
-        }
+		if(!isset($skill_name)) {
+			return FALSE;
+		}
 
-        $skill_id = $this->get_skill_id($skill_name);
+		$skill_id = $this->get_skill_id($skill_name);
 
-        $query = $this->db->select()
-                        ->where('staff_id', $id)
-                        ->where('skill_id', $skill_id)
-                        ->get('staff_skill');
+		$query = $this->db->select()
+						->where('staff_id', $id)
+						->where('skill_id', $skill_id)
+						->get('staff_skill');
 
-        $result = $query->result();
+		$result = $query->result();
 
-        if(isset($result) && !empty($result)) {
-            // Duplicated entry
-            return FALSE;
-        }
+		if(isset($result) && !empty($result)) {
+			// Duplicated entry
+			return FALSE;
+		}
 
-        $data = array(
-            'staff_id'      => $id,
-            'skill_id'      => $skill_id,
-            'skill_level'   => 0
-        );
+		$data = array(
+			'staff_id'      => $id,
+			'skill_id'      => $skill_id,
+			'skill_level'   => 0
+		);
 
-        return $this->db->insert('staff_skill', $data);
-    }
+		return $this->db->insert('staff_skill', $data);
+	}
 
-    public function get_user_skills($id=FALSE) {
+	public function get_user_skills($id=FALSE) {
 
-        // if no id was passed use the current users id
-        $id = isset($id) ? $id : $this->session->userdata('user_id');
+		// if no id was passed use the current users id
+		$id = isset($id) ? $id : $this->session->userdata('user_id');
 
-        $query = $this->db->select()
-                        ->where('staff_id', $id)
-                        ->join('skill', 'skill.skill_id=staff_skill.skill_id')
-                        ->get('staff_skill');
+		$query = $this->db->select()
+						->where('staff_id', $id)
+						->join('skill', 'skill.skill_id=staff_skill.skill_id')
+						->get('staff_skill');
 
-        $result = $query->result();
+		$result = $query->result();
 
-        if(isset($result) && !empty($result)) {
-            return $result;
-        }
-        return FALSE;
-    }
+		if(isset($result) && !empty($result)) {
+			return $result;
+		}
+		return FALSE;
+	}
 
-    public function delete_user_skill($skill_name, $id=FALSE) {
+	public function delete_user_skill($skill_name, $id=FALSE) {
 
-        // if no id was passed use the current users id
-        $id = isset($id) ? $id : $this->session->userdata('user_id');
+		// if no id was passed use the current users id
+		$id = isset($id) ? $id : $this->session->userdata('user_id');
 
-        $skill_id = $this->get_skill_id($skill_name);
+		$skill_id = $this->get_skill_id($skill_name);
 
-        $data = array(
-            'staff_id' => $id,
-            'skill_id' => $skill_id
-        );
+		$data = array(
+			'staff_id' => $id,
+			'skill_id' => $skill_id
+		);
 
-        return $this->db->delete('staff_skill', $data);
-    }
+		return $this->db->delete('staff_skill', $data);
+	}
 
-    // Helper function that gets a skill id from the db
-    public function get_skill_id($skill) {
-        $skill = str_replace(' ', '', $skill);
+	// Helper function that gets a skill id from the db
+	public function get_skill_id($skill) {
+		$skill = str_replace(' ', '', $skill);
 
-        $query = $this->db->select('skill_id')
-                        ->where('name', $skill)
-                        ->limit(1)
-                        ->get('skill');
+		$query = $this->db->select('skill_id')
+						->where('name', $skill)
+						->limit(1)
+						->get('skill');
 
-        $result = $query->row();
+		$result = $query->row();
 
-        if(!isset($result)) {
-            return FALSE;
-        }
+		if(!isset($result)) {
+			return FALSE;
+		}
 
-        return $result->skill_id;
-    }
+		return $result->skill_id;
+	}
 
 }
