aad7ce7d8f251b8021ed112e99a4d054f93d4a9b
octanitro <edh2@hw.ac.uk>
2017-02-07 19:25:56 +0000

    initial start on the project dashboard
    made 2 divs and started on the php coding for echoing staff in project
    and project title

--- application/views/project-dashboard.php
+++ application/views/project-dashboard.php
@@ -1,5 +1,38 @@
 <div id="project-dashboard">
-	
-	<!-- Code here -->
+	<!--TODO:: find variable type for fetching project name, then echo it -->
+	<?php global $projecttitle;/*find variable type */ 
+	echo '<h1> $projecttitle </h1>'; ?>
+	<div class="projectview"> 
+			<?php global $projectdescription;
+			echo '<p> $projectdescription </p>'; ?>
+	</div>
+
+	<div class="projectstaff">
+	<table>
+		<tr>
+			<th>Staff</th>
+			<th>From:</th>
+			<th>To:</th>
+			<th>Daily Rate:</th>
+		</tr>
+		<!-- need to check what format the variable will be returned in, preferably array of a 4 position array, 0=staff name,1= start date, 2=end date, 3=daily rate  -->
+		<?php global $projectstaff;
+			$x=0;
+			$staff = array(projectstaff[x] );
+			
+			while (projectstaff[x] != null){
+				echo '<tr>
+					<th>$staff[0]</th>
+					<th>$staff[1]</th>
+					<th>$staff[2]</th>
+					<th>$staff[3]</th>
+				 </tr>';
+				 $x=$x+1;
+
+		}	
+
+			?>
+	</table>
+	</div>
 
 </div>
