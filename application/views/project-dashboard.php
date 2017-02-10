c26924d6b5d83c3e51945036c3f0183afa73c148
octanitro <edh2@hw.ac.uk>
2017-02-10 00:14:42 +0000

    update to the format
    added the job position to the table

--- application/views/project-dashboard.php
+++ application/views/project-dashboard.php
@@ -15,19 +15,36 @@
 			<th>To:</th>
 			<th>Daily Rate:</th>
 		</tr>
-		<!-- need to check what format the variable will be returned in, preferably array of a 4 position array, 0=staff name,1= start date, 2=end date, 3=daily rate  -->
+		<!-- need to check what format the variable will be returned in, preferably array of a 4 position array, 0=staff name,1= start date, 2=end date, 3=daily rate, 4=job name -->
 		<?php global $projectstaff;
 			$x=0;
+			global $user_group;
 			$staff = array(projectstaff[x] );
 			
 			while (projectstaff[x] != null){
-				echo '<tr>
-					<th>$staff[0]</th>
-					<th>$staff[1]</th>
-					<th>$staff[2]</th>
-					<th>$staff[3]</th>
+				if ($user_group != 1 && $user_group != 2){
+				echo '<tr> 
+					<td>$staff[0]</td>
+					<td>$staff[1]</td>
+					<td>$staff[2]</td>
+					<td>$staff[3]</td>
+				 </tr>
+				 <tr>
+				 <td>$staff[4]</td>
 				 </tr>';
+				 
+				}
+				else {echo '<tr> 
+					<td>$staff[0]</td>
+					<td>$staff[1]</td>
+					<td>$staff[2]</td>
+					</tr>
+					<tr>
+				 <td>$staff[4]</td>
+				 </tr>';
+				}
 				 $x=$x+1;
+				 $staff = array(projectstaff[x] );
 
 		}	
 
