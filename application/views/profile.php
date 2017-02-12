9c311a3d1758a050abaa7fefc123804a3de76bbb
Mobeen Aftab <mobeenaftab1@gmail.com>
2017-02-12 15:49:09 +0000

    profile experience fixed
    styled and prepared function support

--- application/views/profile.php
+++ application/views/profile.php
@@ -2,7 +2,7 @@
 	<div class="container">
 		<div class="row">
 			<!-- A row with 2 columns -->
-			<div class="col-xs-12 col-sm-6 col-md-6 col-lg-5">
+			<div class="col-xs-12 col-sm-6 col-md-6 col-lg-5" id="verticalLine">
 				<h1><?php echo $user->name; ?></h1>
 				<p><?php echo $user->email; ?></p>
 				<p><?php echo ucfirst( $user->group ); ?></p>
@@ -24,7 +24,7 @@
 						<?php echo form_submit('submit', lang('add_label'), "id='skill-submit'");?>
 					<?php echo form_close(); ?>
 
-					<?php 
+					<?php
 						if(isset($user_skills) && $user_skills) {
 							foreach ($user_skills as $skill) {
 								echo '<span class="skill-span">' . $skill->name . '<i class="fa fa-times fa-lg delete-tag" aria-hidden="true"></i></span>';
@@ -36,125 +36,99 @@
 				</div>
 			</section>
 		</div>
-		
+
 		<section id="experience">
 			<div class="row">
-				<h1>Experiences</h1>
-				<button id="experience-edit"><i class="fa fa-pencil" aria-hidden="true"></i>  Edit</button>
+				<div class="col-sm-12 col-md-6">
+					<h1>Experiences</h1>
+					<button id="experience-edit"><i class="fa fa-pencil" aria-hidden="true"></i>  Edit</button>
+				</div>
 			</div>
 			<hr>
 			<div id="experience-set">
 				<?php echo form_open('User/experience_form', array('id' => 'experience-add')); ?>
 					<div id="experience-msg"><?php echo $message;?></div>
-					<p>
-						<label>Start date:</label>
-						<?php echo form_input($start_date,'',"required");?>
-					</p>
-					<p>
-						<label>End date:</label>
-						<?php echo form_input($end_date,'',"required");?>
-					</p>
-					<p>
-						<label>Title:</label>
-						<?php echo form_input($title,'',"required");?>
-					</p>
-					<p>
-						<label>Description:</label>
-						<?php echo form_textarea($description,'',"required");?>
-					</p>
-					<p>
-						<label>Role:</label>
-						<?php echo form_input($role,'',"required");?>
-					</p>
-					<?php echo form_submit('submit', "Add experience","id='experience-submit'");?>
-				<?php echo form_close(); ?>
-				<?php 
+					<div class="container-box container-fluid" id="experience">
+
+						<div class="row"><!--Main row begin-->
+							<div class="col-sm-12 col-md-6">
+								<p>
+									<label>Title:</label> <br>
+									<?php echo form_input($title,'',"required");?>
+								</p>
+							</div>
+							<div class="row"><!--nested row begin-->
+								<div class="col-sm-12 col-md-6">
+									<div class="row date-row">
+										<div class="col-md-3">
+											<p>
+												<label>Start date:</label>
+											</p>
+										</div>
+										<div class="col-md-9">
+											<?php echo form_input($start_date,'',"required");?>
+										</div>
+									</div>
+									<div class="row date-row">
+										<div class="col-md-3">
+											<p>
+												<label>End date:</label>
+											</p>
+										</div>
+										<div class="col-md-9">
+											<?php echo form_input($end_date,'',"required");?>
+										</div>
+									</div>
+								</div>
+							</div><!--Nested row end-->
+						</div><!--Main row end-->
+
+						<div class="row">
+							<div class="col-sm-12 col-md-8">
+								<p>
+									<label>Description:</label> <br>
+									<?php echo form_textarea($description,'',"required");?>
+								</p>
+							</div>
+						</div>
+
+						<div class="row">
+							<div class="col-sm-12 col-md-6">
+								<p>
+									<label>Role:</label> <br>
+									<?php echo form_input($role,'',"required");?>
+								</p>
+							</div>
+							<div class="col-sm-12 col-md-6">
+								<?php echo form_submit('submit', "Add experience","id='experience-submit'");?>
+								<?php echo form_close(); ?>
+							</div>
+						</div>
+
+					<?php
 					if(isset($user_experiences) && $user_experiences) {
 						foreach ($user_experiences as $experience) {
 							echo '<div class="experience-box">
-									<h2>' . $experience->role . '</h2>
-									<p><strong>' 
-										. date_format(date_create($experience->start_date), 'j M Y') . '</strong> until <strong>' 
-										. date_format(date_create($experience->end_date), 'j M Y') . '</strong> at ';
-									if($experience->project_id) {
-										echo '<a href="#">' . $experience->title . '</a>';
-									} else {
-										echo $experience->title;
-									}
-									echo 
-									'</p>
-									<p>'
-										. $experience->description .
-									'</p>
-								</div>';
+							<h2>' . $experience->role . '</h2>
+							<p><strong>'
+							. date_format(date_create($experience->start_date), 'j M Y') . '</strong> until <strong>'
+							. date_format(date_create($experience->end_date), 'j M Y') . '</strong> at ';
+							if($experience->project_id) {
+								echo '<a href="#">' . $experience->title . '</a>';
+							} else {
+								echo $experience->title;
+							}
+							echo
+							'</p>
+							<p>'
+							. $experience->description .
+							'</p>
+							</div>';
 						}
 					} else {
 						echo '<p>No previous experiences to show here</p>';
 					}
-				?>
+					?>
+				</div>
 			</div>
 		</section>
-
-		<!--Exp section-->
-		<div id="Experience">
-			<h1>Experience</h1> <hr>
-
-			<div class="container-box">
-
-				<form class="" method="post">
-					<div class="container-fluid">
-
-						<div class="row">
-							<div class="col-sm-12 col-md-6">
-								<label>Project Title:</label><br>
-								<input type="text" name="" value="" placeholder="Project Title">
-								</div>
-								
-								<div class="row">
-									<div class="col-sm-12 col-md-6">
-										<div class="row date-row">
-											<div class="col-md-3">
-												<label>From:</label>
-											</div>
-										<div class="col-md-9">
-											<input type="date" name="" value="">
-										</div>
-									 </div>
-									 <div class="row date-row">
-										 <div class="col-md-3">
-												<label>To:</label>
-											</div>
-											<div class="col-md-9">
-												<input type="date" name="" value="">
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-
-
-						<div class="row">
-	            <div class="col-sm-12 col-md-8">
-	              <label>Description:</label><br>
-	              <textarea rows="5" cols="10" name="" value="" required></textarea>
-	            </div>
-	          </div>
-
-						<div class="row">
-							<div class="col-xs-12 col-md-6">
-								<label>Skills Developed:</label>
-								<input type="text" name="skillsDeveloped" value="Type skill here...">
-								<a href="#">Add</a>
-							</div>
-							<div class="col-xs-12 col-md-6">
-								<input type="submit" name="addExp" value="Add Experience">
-							</div>
-						</div>
-
-					</div>
-				</form>
-
-			</div>
-		</div>
-	</div>
-</div>
