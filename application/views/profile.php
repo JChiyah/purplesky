b53e3550f717249f627cb8e7e86279c4970cccd3
Mobeen Aftab <mobeenaftab1@gmail.com>
2017-02-19 15:08:54 +0000

    exp edit patch
    --not finished
    --added edit exp section
    --require seperate add and edit button classes
    --add exp commented out for now

--- application/views/profile.php
+++ application/views/profile.php
@@ -39,7 +39,7 @@
 		</div>
 	</div>
 
-	<div class="container">
+	<!--div class="container">
 		<section id="experience">
 			<div class="row">
 				<div class="col-sm-12 col-md-12">
@@ -50,18 +50,18 @@
 			<hr>
 
 			<div id="experience-set">
-				<?php echo form_open('User/experience_form', array('id' => 'experience-add')); ?>
-					<div id="experience-msg"><?php echo $message;?></div>
+				<//?php echo form_open('User/experience_form', array('id' => 'experience-add')); ?>
+					<div id="experience-msg"><//?php echo $message;?></div>
 					<div class="container-box container-fluid" id="experience">
 
-						<div class="row"><!--Main row begin-->
+						<div class="row"><Main row begin>
 							<div class="col-sm-12 col-md-6">
 								<p>
 									<label>Title:</label> <br>
-									<?php echo form_input($title,'',"required");?>
+									<//?php echo form_input($title,'',"required");?>
 								</p>
 							</div>
-							<div class="row"><!--nested row begin-->
+							<div class="row"><nested row begin>
 								<div class="col-sm-12 col-md-6">
 									<div class="row date-row">
 										<div class="col-md-3">
@@ -70,7 +70,7 @@
 											</p>
 										</div>
 										<div class="col-md-9">
-											<?php echo form_input($start_date,'',"required");?>
+											<//?php echo form_input($start_date,'',"required");?>
 										</div>
 									</div>
 									<div class="row date-row">
@@ -80,18 +80,18 @@
 											</p>
 										</div>
 										<div class="col-md-9">
-											<?php echo form_input($end_date,'',"required");?>
+											<//?php echo form_input($end_date,'',"required");?>
 										</div>
 									</div>
 								</div>
-							</div><!--Nested row end-->
-						</div><!--Main row end-->
+							</div><Nested row end>
+						</div><Main row end>
 
-						<div class="row">
+						<!div class="row">
 							<div class="col-sm-12 col-md-8">
 								<p>
 									<label>Description:</label> <br>
-									<?php echo form_textarea($description,'',"required");?>
+									<//?php echo form_textarea($description,'',"required");?>
 								</p>
 							</div>
 						</div>
@@ -100,46 +100,106 @@
 							<div class="col-sm-12 col-md-6">
 								<p>
 									<label>Role:</label> <br>
-									<?php echo form_input($role,'',"required");?>
+									<//?php echo form_input($role,'',"required");?>
 								</p>
 							</div>
 							<div class="col-sm-12 col-md-6">
-								<?php echo form_submit('submit', "Add experience","id='experience-submit'");?>
-								<?php echo form_close(); ?>
+								<//?php echo form_submit('submit', "Add experience","id='experience-submit'");?>
+								<//?php echo form_close(); ?>
 							</div>
 						</div>
 
 				</div>
 			</div>
 		</section>
-	</div>
+	</div-->
 
-	<div class="container container-box container-fluid">
-		<section id="experience">
-			<?php
-			if(isset($user_experiences) && $user_experiences) {
-				foreach ($user_experiences as $experience) {
-					echo '<div class="experience-box">
-					<h2>' . $experience->role . '</h2>
-					<p><strong>'
-					. date_format(date_create($experience->start_date), 'j M Y') . '</strong> until <strong>'
-					. date_format(date_create($experience->end_date), 'j M Y') . '</strong> at ';
-					if($experience->project_id) {
-						echo '<a href="#">' . $experience->title . '</a>';
+	<div class="container">
+		<section class="experience-box" id="experience">
+			<div id="experience-set">
+				<?php echo form_open('User/experience_form', array('id' => 'experience-add')); ?>
+					<div id="experience-msg"><?php echo $message;?></div>
+
+					<div class="row">
+						<div class="col-sm-12 col-md-6">
+							<p>
+								<label>Title:</label> <br>
+								<?php echo form_input($title,'',"required");?>
+								<button id="experience-edit"><i class="fa fa-pencil" aria-hidden="true"></i>  Edit</button>
+							</p>
+						</div>
+						<div class="row">
+							<div class="col-sm-12 col-md-6">
+								<div class="row date-row">
+									<div class="col-md-3">
+										<p>
+											<label>Start date:</label>
+										</p>
+									</div>
+									<div class="col-md-9">
+										<?php echo form_input($start_date,'',"required");?>
+									</div>
+								</div>
+								<div class="row date-row">
+									<div class="col-md-3">
+										<p>
+											<label>End date:</label>
+										</p>
+									</div>
+									<div class="col-md-9">
+										<?php echo form_input($end_date,'',"required");?>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-sm-12 col-md-8">
+							<p>
+								<label>Description:</label> <br>
+								<?php echo form_textarea($description,'',"required");?>
+							</p>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-sm-12 col-md-6">
+							<p>
+								<label>Role:</label> <br>
+								<?php echo form_input($role,'',"required");?>
+							</p>
+						</div>
+						<div class="col-sm-12 col-md-6">
+							<?php echo form_submit('submit', "Add experience","id='experience-submit'");?>
+							<?php echo form_close(); ?>
+						</div>
+					</div>
+					<?php
+						if(isset($user_experiences) && $user_experiences) {
+							foreach ($user_experiences as $experience) {
+								echo '<div class="experience-box">
+									<h2>' . $experience->role . '</h2>
+									<p><strong>'
+										. date_format(date_create($experience->start_date), 'j M Y') . '</strong> until <strong>'
+										. date_format(date_create($experience->end_date), 'j M Y') . '</strong> at ';
+									if($experience->project_id) {
+										echo '<a href="#">' . $experience->title . '</a>';
+									} else {
+										echo $experience->title;
+									}
+								echo
+								'</p>
+								<p>'
+									. $experience->description .
+								'</p>
+							</div>';
+						}
 					} else {
-						echo $experience->title;
+						echo '<p>No previous experiences to show here</p>';
 					}
-					echo
-					'</p>
-					<p>'
-					. $experience->description .
-					'</p>
-					</div>';
-				}
-			} else {
-				echo '<p>No previous experiences to show here</p>';
-			}
-			?>
+				?>
+			</div>
 		</section>
 	</div>
 
