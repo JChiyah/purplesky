eddfc37cf439ea799841450d3acc3c1002b21458
Javier Chiyah <fjc3@hw.ac.uk>
2017-03-04 14:49:21 +0000

    Showing projects dynamically

--- application/views/projects.php
+++ application/views/projects.php
@@ -58,27 +58,38 @@
                 </div>
             </div>
         </a>
-  
+
+    <?php if(isset($projects) && $projects) : ?>
+        <?php foreach($projects as $project) : ?>
+            
+            <a class="container-fluid project-result" href="dashboard/#">
+                <div class="row">
+                    <h3 class="col-md-8"><?= $project->title ?></h3>
+                    <span class="col-md-4 date">
+                        <?= date('j/n/Y', strtotime($project->start_date)) ?> - 
+                        <?= date('j/n/Y', strtotime($project->end_date)) ?>
+                    </span>
+                </div>
+                <hr>
+                <div class="row">
+                    <div class="col-md-10">
+                        <h5><?= $project->manager ?></h5>
+                        <p>Matching skills: 
+                            <span class="skill-span">Java</span>
+                            <span class="skill-span">CSS</span>
+                        </p>
+                    </div>
+                    <div class="col-md-2">
+                        <p class="location">
+                            <i class="fa fa-map-marker fa-lg" aria-hidden="true"></i> <?= $project->location ?></p>
+                        <button class="g-button">View</button>
+                    </div>
+                </div>
+            </a>
+        <?php endforeach ?>
+    <?php else : ?>
+            <p>No projects to show here.<br/>Try applying for project in the <a href="search">search page</a>.</p>
+    <?php endif ?>
+
     </div>
-
-    <?php
-        /** This is the code used to generate multiple blocks of code. Do not use yet **
-            The code inside the "echo" will be changed to match your block of code above 
-
-        if(isset($projects) && $projects) {
-            foreach($projects as $project) {
-                echo '
-                <div class="project-result">
-                    <a href="dashboard/' . $project->project_id . '">
-                    <h3>' . $project->title . '</h3></a>
-                    <span>' . $project->manager . '</span>
-                    <p class="location">' . $project->location . '</p>
-                    <p class="date">' . $project->start_date . ' until ' . $project->end_date . '</p>
-                    <p class="description">' . $project->description . '</p>
-                    <button class="apply-button" id="apply-' . $project->project_id . '">Apply</button>
-                </div>';
-            }
-        }*/
-    ?>
-
 </section>
