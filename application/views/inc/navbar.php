fc1dfb1385daeb22d8cf4c3c849a077b626f691c
Javier Chiyah <fjc3@hw.ac.uk>
2017-03-10 16:52:07 +0000

    Changes to the navbar
    Some style changes (hover colour) and added active links for the page
    the user is currently at

--- application/views/inc/navbar.php
+++ application/views/inc/navbar.php
@@ -8,11 +8,23 @@
          <li><a href="javascript:window.history.go(-1);" class="scroll-button">
                <i class="fa fa-arrow-left" aria-hidden="true"></i>
          </a></li>
-         <li><a href="<?php echo site_url('index') ?>">Home</a></li>
-         <li><a href="<?php echo site_url('projects') ?>">My Projects</a></li>
-         <li><a href="<?php echo site_url('profile') ?>">Profile</a></li>
-         <?php echo in_array(2, $_SESSION['access_level']) ? "<li><a href=" . site_url('create-project') . ">Create Project</a></li>" : ""; ?>
-         <li><a href="<?php echo site_url('search') ?>">Search</a></li>
+         <li>
+            <a href="<?php echo site_url('index') ?>"<?php echo $page_body == 'home' ? ' class="active"' : ''; ?>>Home</a>
+         </li>
+         <li>
+            <a href="<?php echo site_url('projects') ?>"<?php echo $page_body == 'projects' ? ' class="active"' : ''; ?>>My Projects</a>
+         </li>
+         <li>
+            <a href="<?php echo site_url('profile') ?>"<?php echo $page_body == 'profile' ? ' class="active"' : ''; ?>>Profile</a>
+         </li>
+         <?php if(in_array(2, $_SESSION['access_level'])) : ?>
+               <li>
+                  <a href="<?= site_url('create-project') ?>"<?php echo $page_body == 'create-project' ? ' class="active"' : ''; ?>>Create Project</a>
+               </li>
+         <?php endif ?>
+         <li>
+            <a href="<?php echo site_url('search') ?>"<?php echo $page_body == 'search' ? ' class="active"' : ''; ?>>Search</a>
+         </li>
       </ul>
       <a id="logout" href="<?= base_url('auth/logout') ?>"><i class="fa fa-power-off fa-fw"></i></a>
    </div>
