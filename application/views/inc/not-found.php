249e8b30be899415e7102356a534e3112f5a5634
Javier Chiyah <fjc3@hw.ac.uk>
2017-03-18 14:21:00 +0000

    Not found page temporal

---
+++ application/views/inc/not-found.php
@@ -0,0 +1,16 @@
+<section id="page-not-found">
+
+	<h1>The page you requested is not available</h1>
+
+	<h3>The link you followed may be broken, or the page may have been removed.</h3>
+
+	<i class="fa fa-chain-broken fa-5x" aria-hidden="true"></i>
+
+	<div class="row col-xs-12 col-sm-6">
+		
+		<a href="index" class="col-xs-6">Go back to the previous page</a>
+		<a href="index" class="col-xs-6">Go to home</a>
+
+	</div>
+
+</section>
