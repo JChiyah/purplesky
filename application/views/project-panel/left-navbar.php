5b00fa5e0071c60bdc7668a98245bcfbfe02e6a1
Javier Chiyah <fjc3@hw.ac.uk>
2017-03-21 01:27:20 +0000

    And this to make sure everything looks :+1:

--- application/views/project-panel/left-navbar.php
+++ application/views/project-panel/left-navbar.php
@@ -5,8 +5,8 @@
 	<li><button id="tasks">Project Tasks</button></li>
 	<li><button id="applications">Applications</button></li>
 	<li><button id="notification">Add Notification</button></li>
-	<li><button <?php echo isset($action) && $action == 'edit' ? 'class="active"' : '' ?> id="edit">Edit Project</button></li>
-	<li><button id="status">Change Project Status</button></li>
+	<li><button <?php echo $action == 'edit' ? 'class="active"' : '' ?> id="edit">Edit Project</button></li>
+	<li><button <?php echo $action == 'status' ? 'class="active"' : '' ?> id="status">Change Project Status</button></li>
 	<li><button id="task">Add Task</button></li>
 	<li><button id="add">Add Staff</button></li>
 	<li><button id="edit-s">Edit Staff</button></li>
