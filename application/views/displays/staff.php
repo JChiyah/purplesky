53cd1466cf834d0a02385e0bfd9f981b263a8de2
Javier Chiyah <fjc3@hw.ac.uk>
2017-03-27 23:58:48 +0100

    Search style change

--- application/views/displays/staff.php
+++ application/views/displays/staff.php
@@ -2,7 +2,7 @@
 
     <?php foreach($staff as $employee) : ?>
 		
-		<div class="staff-result" id="staff-<?= $employee->id ?>">
+		<div class="staff-result">
 			<h5 class="staff-name"><?= $employee->name ?></h5>
 			<hr>
 			<div class="row">
@@ -22,36 +22,39 @@
 				</div>
 				<p class="col-xs-12 col-sm-6 pay-rate">£<?= $employee->pay_rate ?>/day</p>
 			</div>
-			<?php if(isset($employee->busy) && $employee->busy): ?>
 			<div class="row">
-				<p class="staff-availability">
-					<?php switch($employee->busy) {
-						case 'staff':
-							echo 'Currently working for this project';
-							break;
-						case 'work':
-							echo 'Working on another project';
-							break;
-						case 'training':
-							echo 'Training during this period';
-							break;
-						default:
-							echo 'Not available during this period';
-					} ?>
-				</p>
-			</div>
-			<?php endif ?>
-			<div class="row">
-
-				<button class="col-sm-3 g-button staff-profile">See profile</button>
-				
-				
-					<div class="col-xs-9">
-						
+				<div class="col-md-8">
+					<?php if(isset($employee->busy) && $employee->busy): ?>
+						<div class="row">
+							<p class="staff-availability">
+								<?php switch($employee->busy) {
+									case 'staff':
+										echo 'Currently working for this project';
+										break;
+									case 'work':
+										echo 'Working on another project';
+										break;
+									case 'training':
+										echo 'Training during this period';
+										break;
+									default:
+										echo 'Not available during this period';
+								} ?>
+							</p>
+						</div>
+					<?php endif ?>
+				</div>
+				<div class="col-md-4 right">
+					<p class="location"><i class="fa fa-map-marker fa-lg" aria-hidden="true"></i> <?= $employee->location ?></p>
+					<div class="row">
+						<div class="col-md-6">
+							<button class="g-button user-quick-view">Preview</button>
+						</div>
+						<div class="col-md-6">
+							<a href="<?php echo site_url(strtolower(str_replace(' ','.',$employee->name))); ?>" class="col-sm-3 g-button">See profile</a>
+						</div>
 					</div>
-					<div class="col-xs-6"></div>
-
-					<button type="button" class="col-sm-3 g-button allocate-staff-button">Add to project</button>
+				</div>
 			</div>
 		</div>
 
